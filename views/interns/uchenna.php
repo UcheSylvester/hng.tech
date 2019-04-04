14b89bbbb01a7b9f4fd42c3348814ee5f67ab3f9
uchesylvester <okorocode@gmial.com>
2019-04-04 22:47:48 +0100

    added my uchenna.php to views/interns

---
+++ views/interns/uchenna.php
@@ -0,0 +1,38 @@
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/uchesylvester/image/upload/v1554413965/me.png" alt="Uchenna Sylvester Okoro">
+                        <h6>Uchenna Sylvester Okoro</h6>
+                        <p>Front-end Web Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/SlyvCode" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/UcheSylvester/" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@uchesylvester" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Hello! I am Uchenna, a Front End Web Developer and JavaScript programmer! I started programming in 2018 and it's been all fun ever since! I am interning with hotels.ng to challenge myself and improve my skills in web development</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | CSS | BOOTSTRAP</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
